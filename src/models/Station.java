f402fa7051b0396d97f664bf8d321304ca0837b0
GarroV <62515132+GarroV@users.noreply.github.com>
2022-11-08 10:34:45 +0100

    Update Station.java

--- src/models/Station.java
+++ src/models/Station.java
@@ -32,33 +32,4 @@ public class Station {
         return passengersOnStation;
     }
 
-
-
-    /**public  List<Station> generateStations() {
-
-        List<Station> stations = null;
-        for (int i = 1; i < 6; i++) {
-            Station station = new Station();
-            setStationId(i);
-            station.setPassengersOnStation(station.generatePassengers(station.getPassengerSum()));
-            stations.add(i-1, station);
-
-            System.out.println("Generated station #" + station.getStationId() + " with " + station.getPassengerSum() + "passengers.");
-        }
-        return stations;
-    }**/
-
-    public List<Passenger> generatePassengers(int passengerSum) {
-
-        List<Passenger> passengers = null;
-
-        for (int i = 1; i <= passengerSum; i++) {
-            Passenger passenger = new Passenger();
-            passengers.add(i-1,passenger);
-
-            System.out.println("Generated passenger #" + i + "with final destination " + passenger.getFinalDestination());
-
-        }
-        return passengers;
-    }
 }
