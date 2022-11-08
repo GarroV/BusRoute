d58042ec6a3364a1a2ada849043ad34cc217cde0
GarroV <62515132+GarroV@users.noreply.github.com>
2022-11-08 10:34:39 +0100

    Update Passenger.java

--- src/models/Passenger.java
+++ src/models/Passenger.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Passenger {
@@ -9,6 +11,21 @@ public class Passenger {
 
     public int getFinalDestination() {
         return finalDestination;
+    }
+
+    public static List<Passenger> generatePassengers(int passengerSum) {
+
+        List<Passenger> passengers = new ArrayList<>();
+
+        for (int i = 1; i <= passengerSum; i++) {
+            Passenger passenger = new Passenger();
+            passengers.add(i-1,passenger);
+
+            System.out.println("Generated passenger #" + i + " with final destination " + passenger.getFinalDestination());
+
+        }
+        System.out.println();
+        return passengers;
     }
 
 }
